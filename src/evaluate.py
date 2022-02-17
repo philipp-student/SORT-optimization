5d5213d7bc56c2ba9217ddc8a6b1e58a2a9aa481
student <p.student@isam-ag.de>
2022-02-17 14:20:00 +0100

    Add evaluation metrics recall and precision, evaluate average precision and average recall per dataset

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,6 +6,26 @@ from PIL import Image
 import cv2
 from helpers import xywh2xyxy
 
+def recall(matches, unmatched_groundtruths, unmatched_results):
+    tp = matches.shape[0]
+    fp = unmatched_results.shape[0]
+    fn = unmatched_groundtruths.shape[0]
+    
+    try:
+        return tp / (tp + fn)
+    except ZeroDivisionError:
+        return 0
+
+def precision(matches, unmatched_groundtruths, unmatched_results):
+    tp = matches.shape[0]
+    fp = unmatched_results.shape[0]
+    fn = unmatched_groundtruths.shape[0]
+    
+    try:
+        return tp / (tp + fp)
+    except ZeroDivisionError:
+        return 0
+        
 # Main folder with all datasets.
 MAIN_FOLDER = r"D:\Philipp Student\HRW\Fahrassistenzsysteme 2\Seminararbeit\MOT15\train"
 
@@ -13,20 +33,27 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
     
     # Dataset main folder.
     DATASET_MAIN_FOLDER = os.path.join(MAIN_FOLDER, dataset_folder)
+    
+    # Detections folder.
+    DETECTIONS_FOLDER = os.path.join(DATASET_MAIN_FOLDER, "det")
+    
+    # Groundtruths folder.
+    GROUNDTRUTHS_FOLDER = os.path.join(DATASET_MAIN_FOLDER, "gt")
+    
+    # Groundtruths folder.
+    TRACKS_FOLDER = os.path.join(DATASET_MAIN_FOLDER, "tracks")
 
     # Name of dataset.
     dataset_name = dataset_folder
-
-    # Images of dataset.
-    DATASET_IMAGES_FOLDER = os.path.join(DATASET_MAIN_FOLDER, "img1")
+    print("################# {0} #################".format(dataset_name))
 
     # Groundtruth file (custom format).
-    groundtruth_file = os.path.join(DATASET_MAIN_FOLDER, "gt", "gt_custom.txt")
+    groundtruth_file = os.path.join(GROUNDTRUTHS_FOLDER, "gt_custom.txt")                   # Groundtruths.
 
     # Result file.
-    #result_file = os.path.join(DATASET_MAIN_FOLDER, "tracks", "tracks.txt")                        # Tracks
-    result_file = os.path.join(DATASET_MAIN_FOLDER, "det", "det_custom.txt")                       # Original Detections
-    #result_file = os.path.join(DATASET_MAIN_FOLDER, "det", "YOLOv5_" + dataset_name + ".txt")      # YOLOv5 detections
+    #result_file = os.path.join(TRACKS_FOLDER, "tracks.txt")                                # Tracks
+    #result_file = os.path.join(DETECTIONS_FOLDER, "det_custom.txt")                        # Original Detections
+    result_file = os.path.join(DETECTIONS_FOLDER, "YOLOv5_" + dataset_name + ".txt")        # YOLOv5 detections
     
     ############ Load data ############
     
@@ -36,29 +63,30 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
     # Get number of frames in dataset.
     num_frames = int(np.max(groundtruths[:, 0]))
     
+    precision_sum = 0
+    recall_sum = 0
+    
     for frame_index in range(1, num_frames):
-        
-        # Construct path to current image.
-        IMAGE_PATH = os.path.join(DATASET_IMAGES_FOLDER, format(frame_index, '06d') + ".jpg")
-        
-        # Load current image.
-        IMAGE = np.array(Image.open(IMAGE_PATH))
         
         # Get all groundtruth bounding boxes for current frame.
         groundtruth_boxes = groundtruths[groundtruths[:, 0] == frame_index, 1:]
         
         # Get all result bounding boxes for current frame.
-        result_boxes = results[results[:, 0] == frame_index, 1:]
-        
-        # Display groundtruths and results.
-        IMAGE = draw_bounding_boxes(IMAGE, groundtruth_boxes, (255, 0, 0), False)
-        IMAGE = draw_bounding_boxes(IMAGE, result_boxes, (0, 255, 0), False)
-        
-        # Display image.    
-        cv2.imshow('image', IMAGE)
-        cv2.waitKey(1)                
+        result_boxes = results[results[:, 0] == frame_index, 1:]             
     
         # Compute correspondences of bounding boxes.
-        #matches, unmatched_groundtruths, unmatched_results = associate_detections_to_trackers(groundtruth_boxes, result_boxes, 'iou', 0.3)
+        matches, unmatched_groundtruths, unmatched_results = associate_detections_to_trackers(groundtruth_boxes, result_boxes, 'iou', 0.5)
     
-        pass
+        # Compute evaluation metrics.
+        image_precision = precision(matches, unmatched_groundtruths, unmatched_results)
+        image_recall = recall(matches, unmatched_groundtruths, unmatched_results)
+        
+        # Cumulate evaluation metrics to compute an average.
+        precision_sum += image_precision
+        recall_sum += image_recall
+
+    # Print evaluation metrics.
+    print("Average precision: {0}".format(precision_sum / num_frames))
+    print("Average recall: {0}".format(recall_sum / num_frames))
+
+    pass    
