589bfbfb33c3753891b42c4cc593c6a1c8a333ba
student <p.student@isam-ag.de>
2022-02-21 19:16:45 +0100

    Reimplement mAP

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,7 +7,7 @@ import cv2
 from helpers import xywh2xyxy
 
 CORRESPONDENCE_IOU_THRESHOLD = 0.5
-RECALL_INTERPOLATION_VALUES = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+RECALL_INTERPOLATION_VALUES = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 
 def recall(matches, unmatched_groundtruths, unmatched_results):
     tp = matches.shape[0]
@@ -47,6 +47,8 @@ def mean_average_precision(groundtruth_boxes, result_boxes):
         else:
             prec_rec[result_index, 0] = 1
     
+    
+    ### AT THE END ###
     # Rank values by confidence.
     prec_rec = prec_rec[prec_rec[:, 1].argsort()][::-1]
     
@@ -110,7 +112,7 @@ def get_gt_id_index(correspondences, gt_id):
     return None
 
 MODE = "tracking"
-DETECTOR_TYPE = "original"
+DETECTOR_TYPE = "yolov5"
 
 # Main folder with all datasets.
 MAIN_FOLDER = r"D:\Philipp Student\HRW\Fahrassistenzsysteme 2\Seminararbeit\MOT15\train"
@@ -145,7 +147,7 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
     yolo_detections_file = os.path.join(DETECTIONS_FOLDER, "det_YOLOv5.txt")
 
     # Tracks file.
-    tracks_file = os.path.join(TRACKS_FOLDER, "tracking_iou_yolov5s.txt")
+    tracks_file = os.path.join(TRACKS_FOLDER, "tracking_f1_yolov5s.txt")
     
     ############ Load data ############
     
@@ -160,7 +162,8 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
     num_frames = int(np.max(groundtruths[:, 0]))
     
     # Helpers for detection evaluation.
-    average_precision_sum = 0
+    MAP_precision_recall_table = np.empty((0, 5), dtype=np.float64)
+    MAP_tp_overall = 0
     
     # Helpers for tracking evaluation.
     MOTP_matches_sum = 0
@@ -185,7 +188,28 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
                 detection_boxes = yolo_detections[yolo_detections[:, 0] == frame_index, 1:]
             
             ############# Mean average precision #############            
-            average_precision_sum += mean_average_precision(groundtruth_boxes, detection_boxes)
+            
+            # Compute correspondences of groundtruth bounding boxes to result bounding boxes.
+            matches, unmatched_groundtruths, unmatched_results = associate_detections_to_trackers(groundtruth_boxes[:, 1:], detection_boxes[:, :4], 'iou', CORRESPONDENCE_IOU_THRESHOLD)
+            
+            # Create temporary array to hold results. Format: [Is TP?, Confidence, Precision, Recall, Interpolated Precision]
+            temp_precision_recall_table = np.zeros((detection_boxes.shape[0], 5))
+            
+            # Copy confidences.
+            temp_precision_recall_table[:, 1] = detection_boxes[:, 4]
+            
+            # Check for each result if it's TP.
+            for detection_index in range(detection_boxes.shape[0]):
+                if detection_index in unmatched_results:
+                    temp_precision_recall_table[detection_index, 0] = 0
+                else:
+                    temp_precision_recall_table[detection_index, 0] = 1
+            
+            # Update number of total matches.
+            MAP_tp_overall += matches.shape[0]
+            
+            # Append evaluation result to table.
+            MAP_precision_recall_table = np.vstack([MAP_precision_recall_table, temp_precision_recall_table])                        
             
         elif MODE == 'tracking':    
             # Get all tracks of current frame.            
@@ -240,9 +264,67 @@ for dataset_folder in os.listdir(MAIN_FOLDER):
             # Add number of id switches to sum.
             MOTA_id_switches_sum += num_id_switches
 
+    # Postprocessing of collected evaluation values.
+    if MODE == 'detection':        
+        ############# Mean average precision #############            
+        
+        # Rank values by confidence.
+        MAP_precision_recall_table = MAP_precision_recall_table[MAP_precision_recall_table[:, 1].argsort()][::-1]
+
+        # Compute precision and recall for each result.
+        tp_counter = 0
+        for i in range(MAP_precision_recall_table.shape[0]):
+            if MAP_precision_recall_table[i, 0] == 1:
+                tp_counter += 1
+        
+            # Compute precision.
+            MAP_precision_recall_table[i, 2] = tp_counter / (i + 1)
+            
+            # Compute recall.
+            try:
+                MAP_precision_recall_table[i, 3] = tp_counter / MAP_tp_overall
+            except ZeroDivisionError:
+                MAP_precision_recall_table[i, 3] = 0    
+        
+        # Determine interpolated precision for each result.
+        distinct_recall_values = np.unique(MAP_precision_recall_table[:, 3])    
+        for recall_value in distinct_recall_values:        
+            # Get indices of columns that contain the current recall value.
+            column_indices_with_recall = (MAP_precision_recall_table[:, 3] == recall_value).nonzero()
+            
+            # Select all columns that have the current recall value.
+            columns_with_recall = MAP_precision_recall_table[column_indices_with_recall]
+            
+            # Select maximum precision of these columns.
+            max_prec = columns_with_recall[:, 2].max()
+            
+            # Set the maximum precision as the interpolated precision.
+            MAP_precision_recall_table[column_indices_with_recall, 4] = max_prec
+        
+        # Compute average precision.
+        average_precision_sum = 0
+        for recall_interpolation_value in RECALL_INTERPOLATION_VALUES:
+            # Get columns that have a recall equal to or greater than the current interpolation value.
+            relevant_columns = MAP_precision_recall_table[MAP_precision_recall_table[:, 3] >= recall_interpolation_value]
+            
+            # Check if there are any more relevant colums.
+            if relevant_columns.shape[0] == 0:
+                continue
+            
+            # Sort relevant columns by recall.
+            relevant_columns = relevant_columns[relevant_columns[:, 3].argsort()]
+            
+            # Get representative value for current interpolation value.
+            representative = relevant_columns[0, 4]
+            
+            # Cumulate precision sum.
+            average_precision_sum += representative
+        
+        average_precision = average_precision_sum / 11
+
     # Print evaluation results.
     if MODE == 'detection':        
-        print("Mean average precision: {0}".format(average_precision_sum / num_frames))
+        print("Mean average precision: {0}".format(average_precision))
     elif MODE == 'tracking':
         print("Multi Object Tracking Accuracy (MOTA): {0} (m: {1}, fp: {2}, mme: {3}, gt: {4})".format(1 - ((MOTA_false_negatives_sum + MOTA_false_positives_sum + MOTA_id_switches_sum) / MOTA_groundtruths_sum), 
               MOTA_false_negatives_sum, MOTA_false_positives_sum, MOTA_id_switches_sum, MOTA_groundtruths_sum))
